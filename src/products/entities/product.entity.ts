73749e295168ccac7e66d40e586474a95b29f3ef
Aslly Zuñiga <asllyz2610@gmail.com>
2026-03-02 22:25:58 -0500

    feat: add Product entity with fields and ManyToOne relation to Category using TypeORM

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,29 @@
+import { Category } from 'src/categories/entities/category.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ManyToOne } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    length: 120,
+    nullable: true,
+    default: 'default.svg',
+  })
+  image: string;
+
+  @Column({ type: 'decimal' })
+  price: number;
+
+  @Column({ type: 'int' })
+  inventory: number;
+
+  @ManyToOne(() => Category)
+  category: Category;
+}
