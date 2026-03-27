f54875bfd6a921b1f70b3a16dd538c0d03fb3729
Aslly Zuñiga <asllyz2610@gmail.com>
2026-03-27 08:41:37 -0500

    feat(admin-products): implementa página para crear nuevos productos con formulario reutilizable de variantes

---
+++ frontend/app/admin/products/new/page.tsx
@@ -0,0 +1,23 @@
+import AddVariantForm from "@/components/products/AddVariantForm"
+import VariantForm from "@/components/products/VariantForm"
+import Heading from "@/components/ui/Heading"
+import Link from "next/link"
+
+export default function NewProductPage() {
+    return (
+        <>
+            <Link
+                href="/admin/products?page=1"
+                className="rounded bg-green-400 font-bold py-2 px-10"
+            >
+                Volver
+            </Link>
+
+            <Heading>Nuevo Producto</Heading>
+
+            <AddVariantForm>
+                <VariantForm />
+            </AddVariantForm>
+        </>
+    )
+}
