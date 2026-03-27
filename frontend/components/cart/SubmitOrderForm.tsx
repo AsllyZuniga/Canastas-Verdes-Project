420b46b618d664325edffa5a08d4183fb53d91c4
Aslly Zuñiga <asllyz2610@gmail.com>
2026-03-27 08:50:32 -0500

    feat(orders): implementa formulario de envío de pedidos con useActionState, integración al store y notificaciones con toast

---
+++ frontend/components/cart/SubmitOrderForm.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { submitOrder } from "@/actions/submit-order-action"
+import { useActionState, useEffect } from "react"
+import { useStore } from "@/src/store"
+import { toast } from "react-toastify"
+
+export default function SubmitOrderForm() {
+
+    const total = useStore(state => state.total)
+    const coupon = useStore(state => state.coupon.name)
+    const contents = useStore(state => state.contents)
+    const clearOrder = useStore(state => state.clearOrder)
+
+    const order = {
+        total,
+        coupon,
+        contents
+    }
+
+    const submitOrderWithData = submitOrder.bind(null, order)
+    const [state, dispatch] = useActionState(submitOrderWithData, {
+        errors: [],
+        success: ''
+    })
+
+    useEffect(() => {
+        if (state.errors) {
+            state.errors.forEach(error => toast.error(error))
+        }
+        if (state.success) {
+            toast.success(state.success)
+            clearOrder()
+        }
+    }, [state, clearOrder])
+
+    return (
+        <form action={dispatch}>
+            <input
+                type="submit"
+                className="mt-5 w-full bg-green-700 hover:bg-green-800 cursor-pointer text-white uppercase font-bold p-3 rounded-xl tracking-wider"
+                value="Confirmar Compra"
+            />
+        </form>
+    )
+}
