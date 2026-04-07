750463a2bc6fb0632c83ec3c0b448d73cbab1d6b
Aslly Zuñiga <asllyz2610@gmail.com>
2026-04-07 08:04:13 -0500

    feat(next-config): permitir imágenes remotas desde API local y Cloudinary

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "3001",
+        pathname: "/img/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3001",
+        pathname: "/img/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
