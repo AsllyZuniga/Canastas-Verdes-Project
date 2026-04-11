71a55b15eb11f77a7cb7fa61318e4b1a1a35b0cc
Aslly Zuñiga <asllyz2610@gmail.com>
2026-04-10 21:49:10 -0500

    Update typeorm.config.ts

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,20 +1,22 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
 
 export const typeOrmConfig = (
   configService: ConfigService,
-): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  host: configService.get<string>('DATABASE_HOST'),
-  port: configService.get<number>('DATABASE_PORT'),
-  username: configService.get<string>('DATABASE_USER'),
-  password: configService.get<string>('DATABASE_PASS'),
-  database: configService.get<string>('DATABASE_NAME'),
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  logging: false,
-  entities: [join(__dirname, '../../**/*.entity.{ts,js}')],
-  synchronize: true,
-});
+): TypeOrmModuleOptions => {
+  const databaseSsl = configService.get<string>('DATABASE_SSL');
+  const useSsl = databaseSsl ? databaseSsl === 'true' : true;
+
+  return {
+    type: 'postgres',
+    host: configService.get<string>('DATABASE_HOST'),
+    port: configService.get<number>('DATABASE_PORT'),
+    username: configService.get<string>('DATABASE_USER'),
+    password: configService.get<string>('DATABASE_PASS'),
+    database: configService.get<string>('DATABASE_NAME'),
+    ssl: useSsl ? { rejectUnauthorized: false } : false,
+    logging: false,
+    autoLoadEntities: true,
+    synchronize: true,
+  };
+};
