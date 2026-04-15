04315b74bf9c4431ee3ba3767c97669e0fabe7db
Aslly Zuñiga <asllyz2610@gmail.com>
2026-04-15 09:55:08 -0500

    feat(admin): proteger layout con validación de rol y mejorar el contenedor responsivo del panel administrativo

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -1,16 +1,25 @@
 import AdminNav from "@/components/ui/AdminNav";
 import ToastNotification from "@/components/ui/ToastNotification";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const role = cookieStore.get('user_role')?.value;
+
+  if (role !== 'admin') {
+    redirect('/auth/login?redirectTo=/admin/products?page=1');
+  }
+
   return (
     <>
         <AdminNav />
-        <div className="lg:min-h-screen container mx-auto mt-10 px-10 lg:px-0">
-          <div className="bg-white shadow w-full  mx-auto p-10 my-10 lg:w-3/5" >
+        <div className="lg:min-h-screen mx-auto mt-10 w-full max-w-screen-2xl px-4 sm:px-6 lg:px-8">
+          <div className="bg-white shadow w-full mx-auto p-6 sm:p-8 lg:p-10 my-10" >
             {children}
           </div>
         </div>
